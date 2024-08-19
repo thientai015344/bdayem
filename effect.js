26b1cc0b1680c16d99eec40becf7878b429eccfe
taivt <thientai015344@gmail.com>
2024-08-19 23:25:17 +0700

    new

--- effect.js
+++ effect.js
@@ -176,8 +176,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+			if(i==65){
+				$("p:nth-child(65)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
